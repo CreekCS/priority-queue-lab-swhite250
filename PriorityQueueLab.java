7a936fac1f950ba9360fe3e39e243927f37c123f
Sophia White <sophiaewhite303@gmail.com>
2024-11-05 20:01:19 +0000

    finished code!

--- PriorityQueueLab.java
+++ PriorityQueueLab.java
@@ -1,6 +1,28 @@
 // import statements
+import java.util.*;
+import java.io.*;
 
 public class PriorityQueueLab {
-  
+    public static void main (String[] args){
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        File file = new File("RandIntegers.txt");
+        try{
+            Scanner numsScanner = new Scanner(file);
+            while(numsScanner.hasNextLine()){
+                Integer value = Integer.parseInt(numsScanner.nextLine());
+                pq.add(value);
+            }
+            System.out.println("PriorityQue contents (in natural order):");
+            while (!pq.isEmpty()){
+                    System.out.println(pq);
+                    pq.poll();
+            }   
+        }
+        catch (Exception e){
+            System.out.println("Bro fix your file!");
+            System.out.println(e.getMessage());
+        }
+    }
+        
 }
 
